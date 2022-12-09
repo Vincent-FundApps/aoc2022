5f041266e74b7a1917a447f031c4aae571514117
Vincent Gilbert <51780086+Vincent-FundApps@users.noreply.github.com>
2022-12-09 00:21:59 +0000

    day3

---
+++ day3.py
@@ -0,0 +1,36 @@
+class day3:
+    def __init__(self) -> None:
+        pass
+        
+    def star1(self, input):
+        priorities = 0
+        for rucksack in input.split("\n"):
+            inter=''.join(set(rucksack[:len(rucksack)//2]).intersection(rucksack[len(rucksack)//2:]))
+            if (inter.islower()):
+                priorities += ord(inter)-97+1
+            else:
+                priorities += ord(inter)-65+1+26
+        print(f"The sum of priorities is {priorities}")
+        return priorities
+
+    def star2(self, input):
+        priorities = 0
+        rucksacks = input.split("\n")
+        i=len(rucksacks)
+        while(i>0):
+            inter = ''.join(set((rucksacks[i-1])).intersection(set((rucksacks[i-2])), set((rucksacks[i-3]))))
+            # inter=''.join(set(rucksacks[i-1]).union(set(rucksacks[i-2])).intersection(set(rucksacks[i-3])))
+            if (inter.islower()):
+                priorities += ord(inter)-97+1
+            else:
+                priorities += ord(inter)-65+1+26
+            i-=3
+        print(f"The sum of priorities is {priorities}")
+        return priorities
+
+if __name__ == '__main__':
+    with open("input/day3.star1") as f:
+        input = f.read()
+    d3 = day3()
+    d3.star1(input)
+    d3.star2(input)
---
+++ tests/day3Test.py
@@ -0,0 +1,32 @@
+import unittest
+import sys
+sys.path.insert(1, '..')
+import day3
+
+class TestDay3(unittest.TestCase):
+    def test_star1(self):        
+        input = """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"""
+        d3 = day3.day3()
+        self.assertEqual(d3.star1(input), 157)
+    
+    def test_star2(self):
+        input = """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"""
+        d3 = day3.day3()
+        self.assertEqual(d3.star2(input), 70)
+
+
+if __name__ == '__main__':
+    # unittest.main()
+    TD3 = TestDay3()
+    TD3.test_star1()
+    TD3.test_star2()
